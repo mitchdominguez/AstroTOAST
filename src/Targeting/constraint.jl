35f3510ad0e4d83428deac552ef6f911ddde47cf
Mitch Dominguez <doming18@purdue.edu>
2022-05-14 18:44:02 -0400

    added FXVector

--- src/Targeting/constraint.jl
+++ src/Targeting/constraint.jl
@@ -17,3 +17,22 @@ abstract type Constraint{D} end
 #                                       FX Vector
 # -------------------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------------------- #
+
+struct FXVector{D}
+    Cs::Vector{Constraint} 
+
+    # Constructor
+    function FXVector(constraints...)
+        # Initialize array
+        # fv_array = Vector{FreeVariable}(undef,Base.length(constraints))
+        fx_array = Vector{Constraint}()
+        for i in eachindex(constraints)
+            if ~(typeof(constraints[i])<:Constraint)
+                throw(MethodError(FXVector, constraints[i]))
+            end
+            push!(fx_array, constraints[i])
+        end
+        # new{Base.length(constraints)}(fv_array)
+        new{Base.length(fx_array)}(fx_array)
+    end
+end

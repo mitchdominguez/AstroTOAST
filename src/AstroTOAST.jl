7193e7c0c60c253447401d116224ed5a24d29a93
Mitch Dominguez <doming18@purdue.edu>
2022-05-16 22:21:04 -0400

    export target

--- src/AstroTOAST.jl
+++ src/AstroTOAST.jl
@@ -56,6 +56,6 @@ export evalconstraint, partials, cctspan
 
 include("Targeting/targeter.jl")
 export Targeter
-export X, FX, DFX, getmaxiter, gettol, evalDFXMatrix
+export X, FX, DFX, getmaxiter, gettol, evalDFXMatrix, target
 
 end

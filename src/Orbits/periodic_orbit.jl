81a54429202cf9594e95cba13b52ed79a71362de
Mitch Dominguez <doming18@purdue.edu>
2022-06-03 16:56:32 -0400

    output eigenvectors at specific longitudinal angles

--- src/Orbits/periodic_orbit.jl
+++ src/Orbits/periodic_orbit.jl
@@ -100,7 +100,7 @@ Return the state on the periodic orbit at time T if ndtime = true, and
 returns the state at θ_0 = 2πT/Period if ndtime = false. θ_0 is analagous
 to the longitudinal angle on a torus or the mean anomaly in a conic orbit
 """
-(po::PeriodicOrbit)(T; ndtime=false) = ndtime ? traj(po)(T) : traj(po)(T*period(po)/(2*pi))
+(po::PeriodicOrbit)(T; ndtime=false) = ndtime ? traj(po)(T) : traj(po)(angle2time(po, T))
 
 """
     monodromy(po::PeriodicOrbit)
@@ -150,6 +150,14 @@ time2angle(po::PeriodicOrbit, T::Real) = 2pi*T/period(po)
 Convert a longitudinal angle to ndim time
 """
 angle2time(po::PeriodicOrbit, th::Real) = th*period(po)/(2pi)
+
+"""
+    stm(po::PeriodicOrbit, θ::Real)
+
+Return the state transition matrix at longitudinal angle θ. 
+Calling this function with θ=2π results in the monodromy matrix
+"""
+stm(po::PeriodicOrbit, θ::Real) = stm(traj(po), angle2time(po, θ))
 
 ######################################################
 # Stability, eigendecomposition, manifolds
@@ -267,40 +275,43 @@ function classify_eigs(po::PeriodicOrbit, ϵ=1e-4::Float64)
 end
 
 """
-    unstable_eigs(po::PeriodicOrbit, ϵ=1e-4::Float64)
+    unstable_eigs(po::PeriodicOrbit, θ::Real=0; ϵ::Float64=1e-4)
 
 Return the unstable eigenvalues and eigenvectors of the periodic orbit
+at the specified longitudinal angle θ
 """
-function unstable_eigs(po::PeriodicOrbit; ϵ=1e-4::Float64)
-    # TODO support for if there are no unstable eigenvalues
-    return (eigvals(po)[classify_eigs(po,ϵ)[1]], eigvecs(po)[classify_eigs(po,ϵ)[1]])
+function unstable_eigs(po::PeriodicOrbit, θ::Real=0; ϵ::Float64=1e-4)
+    return (eigvals(po)[classify_eigs(po,ϵ)[1]], map(x->stm(po, θ)*x, eigvecs(po)[classify_eigs(po,ϵ)[1]]))
 end
 
 """
     center_eigs(po::PeriodicOrbit, ϵ=1e-4::Float64)
 
 Return the center eigenvalues and eigenvectors of the periodic orbit (not including unit eigs)
+at the specified longitudinal angle θ
 """
-function center_eigs(po::PeriodicOrbit; ϵ=1e-4::Float64)
-    return (eigvals(po)[classify_eigs(po,ϵ)[2]], eigvecs(po)[classify_eigs(po,ϵ)[2]])
+function center_eigs(po::PeriodicOrbit, θ::Real=0; ϵ::Float64=1e-4)
+    return (eigvals(po)[classify_eigs(po,ϵ)[2]], map(x->stm(po, θ)*x, eigvecs(po)[classify_eigs(po,ϵ)[2]]))
 end
 
 """
     unit_eigs(po::PeriodicOrbit, ϵ=1e-4::Float64)
 
-Return the unit eigenvalues and eigenvectors of the periodic orbit (not including unit eigs)
+Return the unit eigenvalues and eigenvectors of the periodic orbit 
+at the specified longitudinal angle θ
 """
-function unit_eigs(po::PeriodicOrbit; ϵ=1e-4::Float64)
-    return (eigvals(po)[classify_eigs(po,ϵ)[3]], eigvecs(po)[classify_eigs(po,ϵ)[3]])
+function unit_eigs(po::PeriodicOrbit, θ::Real=0; ϵ::Float64=1e-4)
+    return (eigvals(po)[classify_eigs(po,ϵ)[3]], map(x->stm(po, θ)*x, eigvecs(po)[classify_eigs(po,ϵ)[3]]))
 end
 
 """
     stable_eigs(po::PeriodicOrbit, ϵ=1e-4::Float64)
 
 Return the stable eigenvalues and eigenvectors of the periodic orbit
+at the specified longitudinal angle θ
 """
-function stable_eigs(po::PeriodicOrbit; ϵ=1e-4::Float64)
-    return (eigvals(po)[classify_eigs(po,ϵ)[4]], eigvecs(po)[classify_eigs(po,ϵ)[4]])
+function stable_eigs(po::PeriodicOrbit, θ::Real=0; ϵ::Float64=1e-4)
+    return (eigvals(po)[classify_eigs(po,ϵ)[4]], map(x->stm(po, θ)*x, eigvecs(po)[classify_eigs(po,ϵ)[4]]))
 end
 
 """
@@ -322,8 +333,6 @@ function stability_index(po::PeriodicOrbit{D}) where {D}
     return nu, max(map(x->abs(x), nu)...)
 end
 
-# TODO eigenvectors at points along the periodic orbit
-# TODO convert longitudinal angle to time and vice versa
 # TODO periapsis, apoapsis
 # TODO PO continuation
 # TODO export initial conditions for each segment with TOFs

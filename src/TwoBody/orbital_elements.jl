f31e1e22794a1423263aab533215240db9aa4a90
Mitch Dominguez <doming18@purdue.edu>
2023-06-28 22:14:52 -0400

    Created ClassicalOrbitalElements object

--- src/TwoBody/orbital_elements.jl
+++ src/TwoBody/orbital_elements.jl
@@ -2,3 +2,48 @@ using AstroTOAST
 
 abstract type AbstractOrbitalElements end
 
+"""
+    struct ClassicalOrbitalElements
+
+Struct containing the Keplerian (classical) orbital elements: semimajor axis,
+eccentricity, right ascension of the ascending node, inclination, and argument
+of periapsis. All angular orbital elements are stored internally as radians.
+"""
+struct ClassicalOrbitalElements
+    coe::Vector{Float64} 
+
+    """
+        function ClassicalOrbitalElements(a::Real, e::Real, i::Real, Ω::Real, ω::Real; degrees=true)   
+
+        Constructor for ClassicalOrbitalElements. `a` in km, `e` nondimensional, and 
+        `i`, `Ω`, and `ω` are assumed to be degrees by default. Set `degrees=false` to
+        input radians.
+    """
+    function ClassicalOrbitalElements(a::Real, e::Real, Ω::Real, i::Real, ω::Real; degrees=true)
+        if degrees
+            # Angular orbital elements given in degrees
+
+            # Check that inclination is legal
+            if i>90 || i<-90
+                DomainError("Inclination must be between -90 and 90 degrees") |> throw
+            end
+            
+            new([a,e,(deg2rad.([Ω,i,ω]).%(2pi))...])
+        else
+
+            # Check that inclination is legal
+            if i>pi || i<-pi
+                DomainError("Inclination must be between -π and π radians") |> throw
+            end
+            
+            # Angular orbital elements given in radians
+            new([a,e,Ω%(2pi),i,ω%(2pi)])
+        end
+    end
+end
+
+sma(coe::ClassicalOrbitalElements) = coe.coe[1]
+ecc(coe::ClassicalOrbitalElements) = coe.coe[2]
+raan(coe::ClassicalOrbitalElements; degrees=false) = degrees ? rad2deg(coe.coe[3]) : coe.coe[3]
+inc(coe::ClassicalOrbitalElements ; degrees=false) = degrees ? rad2deg(coe.coe[4]) : coe.coe[4]
+aop(coe::ClassicalOrbitalElements ; degrees=false) = degrees ? rad2deg(coe.coe[5]) : coe.coe[5]

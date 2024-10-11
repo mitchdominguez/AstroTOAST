354b1f29f9345c82cc80340033ee8660f627f84f
Mitch Dominguez <doming18@purdue.edu>
2024-10-11 18:06:44 -0400

    fixed time string allocation issue

--- src/time.jl
+++ src/time.jl
@@ -9,9 +9,17 @@ struct UTCEpoch <: AbstractEpoch
     hour::Int64
     minute::Int64
     second::Float64
+    datestr::String
+
+    function UTCEpoch(year::Int64, month::Int64, day::Int64, hour::Int64, minute::Int64, second::Float64)
+        datestr = string(year)*"/"*string(month)*"/"*string(day)*" "*string(hour)*":"*string(minute)*":"*string(second)*" UTC"
+        return new(year, month, day, hour, minute, second, datestr)
+    end
+
 end
 function Base.string(utc::UTCEpoch)
-    datestr = string(utc.year)*"/"*string(utc.month)*"/"*string(utc.day)*" "*string(utc.hour)*":"*string(utc.minute)*":"*string(utc.second)*" UTC"
+    # datestr = string(utc.year)*"/"*string(utc.month)*"/"*string(utc.day)*" "*string(utc.hour)*":"*string(utc.minute)*":"*string(utc.second)*" UTC"
+    return utc.datestr
 end
 
 struct TDBEpoch <: AbstractEpoch
@@ -21,10 +29,17 @@ struct TDBEpoch <: AbstractEpoch
     hour::Int64
     minute::Int64
     second::Float64
+    datestr::String
+
+    function TDBEpoch(year::Int64, month::Int64, day::Int64, hour::Int64, minute::Int64, second::Float64)
+        datestr = string(year)*"/"*string(month)*"/"*string(day)*" "*string(hour)*":"*string(minute)*":"*string(second)*" TDB"
+        return new(year, month, day, hour, minute, second, datestr)
+    end
 end
 
 function Base.string(tdb::TDBEpoch)
-    datestr = string(tdb.year)*"/"*string(tdb.month)*"/"*string(tdb.day)*" "*string(tdb.hour)*":"*string(tdb.minute)*":"*string(tdb.second)*" TDB"
+    # datestr = string(tdb.year)*"/"*string(tdb.month)*"/"*string(tdb.day)*" "*string(tdb.hour)*":"*string(tdb.minute)*":"*string(tdb.second)*" TDB"
+    return tdb.datestr
 end
 
 SPICE.str2et(epoch::AbstractEpoch) = str2et(string(epoch))
